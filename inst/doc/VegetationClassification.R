d3e0f67d1484212e97b62f57363bc4b3e4aa4538
Miquel De Cáceres <miquelcaceres@gmail.com>
2021-06-01 12:20:02 +0000

    version 2.0.0

--- inst/doc/VegetationClassification.R
+++ inst/doc/VegetationClassification.R
@@ -1,5 +1,4 @@
 ### R code from vignette source 'VegetationClassification.Rnw'
-### Encoding: ISO8859-1
 
 ###################################################
 ### code chunk number 1: VegetationClassification.Rnw:21-22
